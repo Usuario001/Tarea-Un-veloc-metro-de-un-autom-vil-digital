9caff818d28b2c361a120b3754be33fb853c4e75
Jose Manuel <patinogutierrez@icloud.com>
2016-02-21 16:50:55 -0600

    Initial Commit

---
+++ Tarea Velocimetro de un auto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tarea Velocimetro de un auto
+//
+//  Created by José on 21/02/16.
+//  Copyright © 2016 José. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Tarea Velocimetro de un autoTests/Tarea_Velocimetro_de_un_autoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tarea_Velocimetro_de_un_autoTests.swift
+//  Tarea Velocimetro de un autoTests
+//
+//  Created by José on 21/02/16.
+//  Copyright © 2016 José. All rights reserved.
+//
+
+import XCTest
+@testable import Tarea_Velocimetro_de_un_auto
+
+class Tarea_Velocimetro_de_un_autoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
